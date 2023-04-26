2a2ae3e472ea40d7caa25ab70caeb0d020f4e378
CristianAbelRobles <cristianrobles@outlook.com>
2023-04-26 16:24:46 -0300

    se dan estilos a modal, se agrega pagina 404, se agregan toast, se agrega un boton para subir al top

---
+++ js/main.js
@@ -0,0 +1,38 @@
+/* BOTON GO TOP */
+
+window.onscroll = function(){
+    if (document.documentElement.scrollTop > 100){
+        document.querySelector(".go-top-container").classList.add("showButtom");
+    } else {
+        document.querySelector(".go-top-container").classList.remove("showButtom");
+    }
+}
+
+document.querySelector(".go-top-container").addEventListener("click", ()=>{
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    })
+});
+
+/* TOAST DE BOOTSTRAP */
+
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+    toastTrigger.addEventListener('click', () => {
+        toastBootstrap.show()
+    })
+}
+
+const toastFormTrigger = document.getElementById('oradorToastBtn')
+const toastLiveForm = document.getElementById('oradorToast')
+
+if (toastFormTrigger) {
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveForm)
+    toastFormTrigger.addEventListener('click', () => {
+        toastBootstrap.show()
+    })
+}
