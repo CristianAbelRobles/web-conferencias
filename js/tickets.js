002476606e81030a9733595e5e856053e89b322d
CristianAbelRobles <cristianrobles@outlook.com>
2023-05-22 01:25:21 -0300

    formulariod de compra terminado con msjs de error y validacion, iconos de validacion funcionando, boton comprar y borrar funcionales, se cargo el formulario tambien en el modal del index

--- js/tickets.js
+++ js/tickets.js
@@ -1,16 +1,16 @@
 let formulario = document.getElementById("formulario");
 let inputs = document.querySelectorAll("#formulario input");
-
-let btnResumen = document.querySelector("#btn-resumen");
+let btnComprar = document.getElementById("btn-buy");
+let btnBorrar = document.getElementById("btn-clear");
 let boxTotal = document.querySelector("#total-box");
-let buyName = document.querySelector("#buy-name");
-let buyLastName = document.querySelector("#buy-lastname");
+let buyName = document.querySelector("#nombre");
+let buyLastName = document.querySelector("#apellido");
 let buyEmail = document.querySelector("#buy-email");
-let buyAmount = document.querySelector("#buy-amount");
+let buyAmount = document.querySelector("#amount");
 let buyCategory = document.querySelector("#buy-category");
 let totalCost = 0;
 
-// EXPRESIONES REGULARES
+// EXPRESIONES REGULARES PARA VALIDAR LOS INPUTS
 const expresiones = {
 	nombre: /^[a-zA-ZÀ-ÿ\s]{4,40}$/, // Letras y espacios, pueden llevar acentos.
     apellido: /^[a-zA-ZÀ-ÿ\s]{4,40}$/, // Letras y espacios, pueden llevar acentos.
@@ -21,22 +21,24 @@ const campos = {
 	nombre: false,
 	apellido: false,
 	correo: false,
+    cantidad: false
 }
 
 // VALIDAMOS LOS INPUTS
 const validarFormulario = (e) => {
     switch (e.target.name){
         case "buy-name":
-            validarCampo(expresiones.nombre, e.target, "buy-name");
+            validarCampo(expresiones.nombre, e.target, "nombre");
         break;
         case "buy-lastname":
-            validarCampo(expresiones.apellido, e.target, "buy-lastname");
+            validarCampo(expresiones.apellido, e.target, "apellido");
         break;
         case "buy-email":
-            validarCampo(expresiones.correo, e.target, "buy-email");
+            validarCampo(expresiones.correo, e.target, "correo");
         break;
         case "buy-amount":
             validarCantidad();
+            mostrarTotal();
         break;
     }
 }
@@ -46,10 +48,12 @@ const validarCampo = (expresion, input, campo) => {
         document.querySelector(`#${campo}`).classList.add("is-valid");
         document.querySelector(`#${campo}`).classList.remove("is-invalid");
         document.querySelector(`#grupo__${campo} .msjError`).classList.add("d-none");
+        campos[campo] = true ;
     } else {
         document.querySelector(`#${campo}`).classList.add("is-invalid");
         document.querySelector(`#${campo}`).classList.remove("is-valid");
         document.querySelector(`#grupo__${campo} .msjError`).classList.remove("d-none");
+        campos[campo] = false ;
     }
 }
 
@@ -58,30 +62,24 @@ inputs.forEach((input) => {
 	input.addEventListener('blur', validarFormulario); // EVENTO QUE SE EJECUTA CUANDO HAGO CLICK FUERA DEL INPUT
 });
 
+
+
 // FUNCION PARA VALIDAR EL INPUT DE CANTIDAD
 const validarCantidad = () =>  {
     if (buyAmount.value >= 1) {
         buyAmount.classList.add("is-valid");
         buyAmount.classList.remove("is-invalid");
-        document.querySelector("#grupo__buy-amount .msjError").classList.add("d-none");
+        document.querySelector("#grupo__amount .msjError").classList.add("d-none");
+        campos["cantidad"] = true;
     } else {
         buyAmount.classList.add("is-invalid");
         buyAmount.classList.remove("is-valid");
-        document.querySelector("#grupo__buy-amount .msjError").classList.remove("d-none");
+        document.querySelector("#grupo__amount .msjError").classList.remove("d-none");
+        campos["cantidad"] = false;
     }
 }
 
-// FUNCION PARA RESETEAR LOS CAMPOS LUEGO DE HACER UNA RESERVA
-const resetCampos = () => {
-    campos.nombre = false;
-    campos.apellido = false;
-    campos.correo = false;
-    campos.cantidad = false;
-}
-
-// FUNCION PARA CALCULAR EL DESCUENTO
-btnResumen.addEventListener("click", function(e){
-    e.preventDefault();
+const mostrarTotal = () => {
     switch (buyCategory.value) {
         case "1":
             //general 200 sin descuento
@@ -103,6 +101,58 @@ btnResumen.addEventListener("click", function(e){
           //Declaraciones ejecutadas cuando ninguno de los valores coincide con el valor de la expresión
             break;
     }
-    boxTotal.innerHTML += `precio total es ${totalCost} el select es ${buyAmount.value}`;
+    boxTotal.innerHTML = `Precio total es ${totalCost}`;
     totalCost = 0;  //reseteamos el costo total para que no influya en una nueva compra
+}
+
+// FUNCION PARA RESETEAR LOS CAMPOS LUEGO DE HACER UNA RESERVA
+const resetCampos = () => {
+    campos.nombre = false;
+    campos.apellido = false;
+    campos.correo = false;
+    campos.cantidad = false;
+}
+
+// FUNCION PARA EL BOTON COMPRAR
+btnComprar.addEventListener("click", function(e){
+    e.preventDefault();
+
+    console.log(campos);
+    
+    if ( campos.nombre && campos.apellido && campos.correo && campos.cantidad ){
+        formulario.reset();
+        document.getElementById("msj__box-valid").classList.remove("d-none");
+
+        // LUEGO DE 5 SEGUNDOS QUITO EL MENSAJE DE COMPRA
+        setTimeout(() => {
+            document.getElementById("msj__box-valid").classList.add("d-none");
+        }, "5000");
+
+        // DESACTIVO TODOS LOS ICONOS DE LAS VALIDACIONES
+        document.querySelectorAll(".is-valid").forEach((icono)=>{
+            icono.classList.remove("is-valid");
+        });
+
+        // RESETEO TODOS LOS CAMPOS PARA QUE NO SE PUEDAN SEGUIR ENVIANDO PETICIONES LUEGO DE ENVIAR UNA Y TODOS LOS CAMPOS SEAN VALIDOS
+        resetCampos();
+    } else {
+        document.getElementById("msj__box-error").classList.remove("d-none");
+
+        // LUEGO DE 5 SEGUNDOS QUITO EL MENSAJE DE ERROR
+        setTimeout(() => {
+            document.getElementById("msj__box-error").classList.add("d-none");
+        }, "5000");
+    }
+});
+
+btnBorrar.addEventListener("click", function(e){
+    e.preventDefault();
+
+    // DESACTIVO TODOS LOS ICONOS DE LAS VALIDACIONES
+    document.querySelectorAll(".is-valid").forEach((icono)=>{
+        icono.classList.remove("is-valid");
+    });
+    document.querySelectorAll(".is-invalid").forEach((icono)=>{
+        icono.classList.remove("is-invalid");
+    });
 });
